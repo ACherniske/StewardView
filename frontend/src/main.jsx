de8e3e5bd56a6d406aa238bda800c302e264f090
ACherniske <apcherniske@gmail.com>
2026-01-02 14:47:33 -0500

    refactor: Remove unused App component import for code cleanup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
-import App from './App.jsx';
 import LandingPage from './pages/LandingPage.jsx';
 import CapturePage from './pages/CapturePage.jsx';
 import OrganizationPage from './pages/OrganizationPage.jsx';
